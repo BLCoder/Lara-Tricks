1b78e7190056151c8745a49d2b1c9120a6ce480f
A26-Lab <a26.lab.syl@gmail.com>
2019-12-06 01:31:49 +0600

    add

--- database/migrations/2019_10_27_121656_add_codes_to_posts_table.php
+++ database/migrations/2019_10_27_121656_add_codes_to_posts_table.php
@@ -14,7 +14,7 @@ class AddCodesToPostsTable extends Migration
     public function up()
     {
         Schema::table('posts', function (Blueprint $table) {
-            $table->text('codes')->nullable();
+            $table->text('codes',5000)->nullable();
         });
     }
 
